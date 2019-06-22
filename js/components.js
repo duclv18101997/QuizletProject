748db535f004f1caff0ee4d480ca9bb44139d097
kunsieunhan1806 <41021072+kunsieunhan1806@users.noreply.github.com>
2019-06-22 21:44:09 +0700

    hoàn thành bổ sung trang welcome page thêm 1 số tính năng của AOS

--- js/components.js
+++ js/components.js
@@ -2,11 +2,34 @@ const components = {};
 
 components.welcomePageQuiz = `
 <div class="main-content">
-            <div class="main-Introduce">
+            <div class="main-Introduce " data-aos='fade-up' data-aos-anchor-placement="top-center">
                 <h1 class="Introduce-Title">Cách tốt nhất để đạt điểm tuyệt đối bài thi.</h1>
                 <p class="Introduce-Content">Hãy bắt đầu tạo các thẻ ghi nhớ dành riêng cho bạn. Biến khoảng cách bạn và thế giới gần nhau hơn</p>
                 <button class="btn" id="btn-add-new">Bắt đàu học</button>
             </div>
             <div></div>
+        </div>
+        <div class="student-teacher" >
+            <div class="main-content-student">
+                <div class="content-introduce">
+                    <div class="main-Introduce" data-aos="fade-right">
+                        <h3 class="Introduce-Title">Học theo cách thông minh hơn.</h3>
+                        <p class="Introduce-Content">Hơn 90% học sinh sử dụng Quiz cho biết họ đã cải thiện được điểm số. Hãy xem các công cụ đơn giản của Quiz có thể giúp bạn học bất cứ nội dung gì, từ đại số nhập môn, lịch sử Việt Nam, cho đến kinh tế lượng cấp đại học — và hơn
+                            thế nữa
+                        </p>
+                    </div>
+                </div>
+                <div class="image-student" data-aos="fade-left"><img class="" src="../QuizletProject/img/students.jpg"></div>
+            </div>
+            <div class="main-content-student-right">
+                <div class="image-student-left" data-aos="fade-right"><img class="" src="../QuizletProject/img/teachers.jpg"></div>
+                <div class="content-introduce-right">
+                    <div class="main-Introduce-right" data-aos="fade-left">
+                        <h3 class="Introduce-Title">Chia sẻ kiến thức .</h3>
+                        <p class="Introduce-Content">Tạo động lực và thu hút học sinh học tập bằng Quiz. Tạo học phần cho lớp học của bạn, hợp tác với giáo viên khác, chơi Quiz và cung cấp tài liệu cho học sinh để các em học theo phương pháp hiệu quả và vui hơn.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
 `;
